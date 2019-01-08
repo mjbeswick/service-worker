7cdd208842d81fde673055cafb46847942c18d2e
Michael Beswick <mjbeswick@gmail.com>
2019-01-08 11:54:23 +0000

    Added service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+if (process.env.NODE_ENV === 'development') {
+  import('react-lumberjack');
+}
+
+/* navigator is a WEB API that allows scripts to register themselves and carry out their activities. */
+if ('serviceWorker' in navigator) {
+  console.log('Service Worker is supported in your browser');
+  /* register method takes in the path of service worker file and returns a promises, which returns the registration object */
+  navigator.serviceWorker.register('./service-worker.js').then(registration => {
+    console.log('Service Worker is registered!');
+  });
+} else {
+  console.log('Service Worker is not supported in your browser');
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
